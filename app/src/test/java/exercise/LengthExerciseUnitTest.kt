d67917100700a785b8fda54b192e70dac2c36c4e
jackson <jackson@luanas-MacBook-Air.local>
2024-06-09 13:37:16 -0300

    first exercises

---
+++ app/src/test/java/exercise/LengthExerciseUnitTest.kt
@@ -0,0 +1,39 @@
+package exercise
+
+import com.example.myapplication.exercies.LengthExercise
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class LengthExerciseUnitTest {
+
+    @Test
+    fun testLength_EmptyString() {
+        val exercise = LengthExercise()
+        val result = exercise.length("")
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun testLength_MultipleCharacterString() {
+        val exercise = LengthExercise()
+
+        val result = exercise.length("Hello")
+        assertEquals(5, result)
+    }
+
+    @Test
+    fun testLength_WhitespaceString() {
+        val exercise = LengthExercise()
+
+        val result = exercise.length("   ")
+        assertEquals(3, result) // Whitespace characters are also counted
+    }
+
+    @Test
+    fun testLength_SpecialCharacterString() {
+        val exercise = LengthExercise()
+
+        val result = exercise.length("!@#$%")
+        assertEquals(5, result)
+    }
+}
